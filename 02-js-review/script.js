1214be149343d6c94e7c90c39f9777beedf145b3
Mikiyas Alemayehu <153812380+zemike47@users.noreply.github.com>
2024-03-20 12:30:56 +0300

    Update script.js

--- 02-js-review/script.js
+++ 02-js-review/script.js
@@ -218,3 +218,21 @@ pagesAllbooks;
 const shortedBypages = book.slice().sort((a, b) => a.pages - b.pages);
 shortedBypages;
 
+fetch("https://jsonplaceholder.typicode.com/todos")
+  .then((res) => res.json)
+  .then((data) => console.log(data));
+console.log("jonas");
+*/
+async function getTodos() {
+  const res = await fetch("https://jsonplaceholder.typicode.com/todos");
+  const data = await res.json();
+  console.log(data);
+
+  return data;
+}
+
+const todos = getTodos();
+console.log(todos);
+
+console.log("mike");
+
