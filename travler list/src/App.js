fe011057540e100ee3a38e3e192a92d08d6fd2ec
Mikiyas Alemayehu <153812380+zemike47@users.noreply.github.com>
2024-04-05 09:36:46 +0300

    Update App.js

--- travler list/src/App.js
+++ travler list/src/App.js
@@ -8,8 +8,7 @@ const initialItems = [
   { id: 1, description: "Passports", quantity: 2, packed: false },
   { id: 2, description: "Socks", quantity: 12, packed: false },
   { id: 3, description: "charger", quantity: 2, packed: true },
-];
-*/
+];*/
 export default function App() {
   const [items, setItems] = useState([]);
 
